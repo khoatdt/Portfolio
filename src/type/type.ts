b903f0f938a967ed6402e9c1bd0d70d19cb90fd3
khoatdt <88079474+khoatdt@users.noreply.github.com>
2024-12-07 11:31:55 +0700

    update and fix error

---
+++ src/type/type.ts
@@ -0,0 +1,27 @@
+export interface Image {
+    id: number;
+    src: string;
+}
+
+export interface feature {
+    name: string;
+    description: string;
+    icon: any;
+}
+
+export interface Technical {
+    name: string;
+    icon: any;
+}
+
+export interface Project {
+    id: number;
+    title: string;
+    description: string;
+    thumbnail: string;
+    role: string[];
+    objectives: string;
+    features?: feature[];
+    technical: Technical[];
+    screenshots?: Image[];
+}
