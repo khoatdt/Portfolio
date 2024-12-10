0096b4ab078b6ca6efeefd3a9264b7e42c091fe1
khoatdt <88079474+khoatdt@users.noreply.github.com>
2024-12-10 16:45:58 +0700

    add detail-page, edit image

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,14 +1,26 @@
 "use client";
 import { CardProject } from "@/components/ui/card-project";
 import { listProjects } from "@/data/projects";
+import { Project } from "@/type/type";
 
 const project = () => {
   return (
-    <>
-      {listProjects.map((project: any, index: number) => (
-        <CardProject key={index} project={project} />
-      ))}
-    </>
+    <div className="pt-20 container">
+      <div className="place-items-center">
+        <h1
+          className={
+            "text-3xl md:text-4xl lg:text-5xl font-bold bg-slate-50 border-slate-950 border-2 border-r-8 border-b-8  w-fit p-3 rounded-lg  dark:text-white text-black "
+          }
+        >
+          Project
+        </h1>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full">
+        {listProjects.map((project: Project, index: number) => (
+          <CardProject key={index} project={project} />
+        ))}
+      </div>
+    </div>
   );
 };
 
