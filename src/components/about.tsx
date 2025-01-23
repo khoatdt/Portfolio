c18d6c2f55246671ffecf703c3c61685c89443af
khoatdt <88079474+khoatdt@users.noreply.github.com>
2025-01-23 08:55:31 +0700

    edit css about.css

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -8,12 +8,12 @@ export function About() {
     <>
       <div className="container overflow-hidden">
         <div className="flex flex-row justify-between w-full gap-20">
-          <div className="space-y-4">
+          <div className="space-y-4 w-11/12">
             <h1 className="text-3xl md:text-5xl font-bold border-b-4 border-black pb-4 text-black dark:text-white">
               What I do
             </h1>
 
-            <p className="text-lg md:text-2xl font-medium pt-4  tracking-wide">
+            <p className="text-lg md:text-2xl font-medium pt-4 tracking-wide" style={{ lineHeight: "1.75" }}>
               I am a <Highlight> frontend developer</Highlight> specializing in
               <Highlight>ReactJS, NextJS, Svelte, and Angular</Highlight>, with
               a strong passion for crafting visually stunning and highly
@@ -30,8 +30,8 @@ export function About() {
             <ExploreButton />
           </div>
 
-          <div className="card bg-base-100 w-full h-96 shadow-xl hidden xl:block border-2 border-black border-r-8 border-b-8">
-            <figure className="px-6 pt-8">
+          <div className="card bg-base-100 w-2/6 h-fit shadow-xl hidden xl:block border-2 border-black border-r-8 border-b-8">
+            <figure className="px-6 py-8 ">
               <Image
                 src={"/PortfolioImage.jpg"}
                 alt="PortfolioImage"
